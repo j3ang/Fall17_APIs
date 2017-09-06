3a34bb184522ee6b4511c10d3e1827231e4060de
jwang206 <jwang206@hawk.iit.edu>
2017-09-06 12:33:09 -0500

    ch3 minimum excercise

---
+++ eloquent03_functions/03.js
@@ -0,0 +1,19 @@
+
+function min(a,b){
+	if(a<b)
+	{
+	return a;
+	}
+	if(a==b)
+	{
+	console.log("They equals");
+	}
+	else
+	{
+	return b;
+	}
+}
+
+console.log(min(0,1));
+console.log(min(0,-10));
+
